5b1f1c3bde999b5c1e0ac6fddd5e6d08e5fa6d8e
wang-lai-chen <120321393+wang-lai-chen@users.noreply.github.com>
2024-03-11 23:01:13 +0800

    退出功能

--- src/layout/components/Topbar/Userinfo.vue
+++ src/layout/components/Topbar/Userinfo.vue
@@ -17,24 +17,24 @@
  *          \  \ `_.   \_ __\ /__ _/   .-` /  /
  *      =====`-.____`.___ \_____/___.-`___.-'=====
  *                        `=---='
- * 
- * 
+ *
+ *
  *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * 
+ *
  *            佛祖保佑       永不宕机     永无BUG
- * 
- *        佛曰:  
- *                写字楼里写字间，写字间里程序员；  
- *                程序人员写程序，又拿程序换酒钱。  
- *                酒醒只在网上坐，酒醉还来网下眠；  
- *                酒醉酒醒日复日，网上网下年复年。  
- *                但愿老死电脑间，不愿鞠躬老板前；  
- *                奔驰宝马贵者趣，公交自行程序员。  
- *                别人笑我忒疯癫，我笑自己命太贱；  
+ *
+ *        佛曰:
+ *                写字楼里写字间，写字间里程序员；
+ *                程序人员写程序，又拿程序换酒钱。
+ *                酒醒只在网上坐，酒醉还来网下眠；
+ *                酒醉酒醒日复日，网上网下年复年。
+ *                但愿老死电脑间，不愿鞠躬老板前；
+ *                奔驰宝马贵者趣，公交自行程序员。
+ *                别人笑我忒疯癫，我笑自己命太贱；
  *                不见满街漂亮妹，哪个归得程序员？
- * 
- * @Descripttion: 
- * @version: 
+ *
+ * @Descripttion:
+ * @version:
  * @Date: 2021-04-20 11:06:21
  * @LastEditors: huzhushan@126.com
  * @LastEditTime: 2022-09-27 17:56:21
@@ -68,12 +68,15 @@
     </template>
   </el-dropdown>
 </template>
+
 <script>
-import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 import { useUserinfo } from '@/components/Avatar/hooks/useUserinfo'
 import LockModal from './LockModal.vue'
 import { useApp } from '@/pinia/modules/app'
+
+import { defineComponent , getCurrentInstance} from 'vue'
+import { Logout } from '@/api/login'
 
 export default defineComponent({
   components: {
@@ -84,11 +87,19 @@ export default defineComponent({
 
     const { userinfo } = useUserinfo()
 
+    const { proxy: ctx } = getCurrentInstance() // 可以把ctx当成vue2中的this
+
     // 退出
-    const logout = () => {
-      // 清除token
-      useApp().clearToken()
-      router.push('/login')
+    const logout = async () => {
+      const { code ,  data , message } = await Logout() ;
+      if(code == 200) {
+        // 清除token
+        useApp().clearToken()
+        router.push('/login')
+      }else {
+        ctx.$message.error(message)
+      }
+
     }
 
     return {
