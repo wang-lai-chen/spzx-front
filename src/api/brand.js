d6792aa29bfab8132c7e30a5c7c86a9e7ffe16b8
wang-lai-chen <120321393+wang-lai-chen@users.noreply.github.com>
2024-03-14 12:10:43 +0800

    品牌数据添加

--- src/api/brand.js
+++ src/api/brand.js
@@ -9,3 +9,29 @@ export const GetBrandPageList = (page, limit) => {
     method: 'get'
   })
 }
+
+// 保存品牌
+export const SaveBrand = brand => {
+  return request({
+    url: `${base_api}/save`,
+    method: 'post',
+    data: brand,
+  })
+}
+
+// 修改信息
+export const UpdateBrandById = brand => {
+  return request({
+    url: `${base_api}/updateById`,
+    method: 'put',
+    data: brand,
+  })
+}
+
+// 根据id删除品牌
+export const DeleteBrandById = id => {
+  return request({
+    url: `${base_api}/deleteById/${id}`,
+    method: 'delete',
+  })
+}
--- src/views/product/brand.vue
+++ src/views/product/brand.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="tools-div">
-    <el-button type="success" size="small">添 加</el-button>
+    <el-button type="success" size="small" @click="addShow">添 加</el-button>
   </div>
+
+  <el-dialog v-model="dialogVisible" title="添加或修改" width="30%">
+    <el-form label-width="120px">
+      <el-form-item label="品牌名称">
+        <el-input v-model="brand.name"/>
+      </el-form-item>
+      <el-form-item label="品牌图标">
+        <el-upload
+          class="avatar-uploader"
+          action="http://localhost:8501/admin/system/fileUpload"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :headers="headers"
+        >
+          <img v-if="brand.logo" :src="brand.logo" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="saveOrUpdate">提交</el-button>
+        <el-button @click="dialogVisible = false">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 
   <el-table :data="list" style="width: 100%">
     <el-table-column prop="name" label="品牌名称" />
@@ -33,7 +57,9 @@
 
 <script setup>
 import {onMounted, ref} from 'vue'
-import {GetBrandPageList} from "@/api/brand";
+import { ElMessage, ElMessageBox } from 'element-plus'
+import {GetBrandPageList, DeleteBrandById, SaveBrand, UpdateBrandById} from "@/api/brand";
+import { useApp } from '@/pinia/modules/app'
 ////////////////////////////////品牌数据分页
 // 定义表格数据模型
 const list = ref([])
@@ -65,6 +91,66 @@ const fetchData = async ()=>{
   list.value = data.list
   total.value = data.total
 }
+
+
+////////////////////////////////品牌数据添加
+const headers = {
+  // 从pinia中获取token，在进行文件上传的时候将token设置到请求头中
+  token: useApp().authorization.token
+}
+// 定义提交表单数据模型
+const defaultForm = {
+  id: '',
+  name: '',
+  logo: ""
+}
+const brand = ref(defaultForm)
+const dialogVisible = ref(false)
+
+// 显示添加品牌表单
+const addShow = () => {
+  brand.value = {}
+  dialogVisible.value = true
+}
+
+//上传
+const handleAvatarSuccess = (response) => {
+  brand.value.logo = response.data
+}
+
+// 保存数据
+const saveOrUpdate = () => {
+  if (!brand.value.id) {
+    saveData()
+  }
+}
+
+// 新增
+const saveData = async () => {
+  await SaveBrand(brand.value)
+  dialogVisible.value = false
+  ElMessage.success('操作成功')
+  fetchData()
+}
+
+////////////////////////////////品牌数据修改
+// 保存数据
+const saveOrUpdate = () => {
+  if (!brand.value.id) {
+    saveData()
+  } else {
+    updateData()
+  }
+}
+
+// 修改
+const updateData = async () => {
+  await UpdateBrandById(brand.value)
+  dialogVisible.value = false
+  ElMessage.success('操作成功')
+  fetchData()
+}
+////////////////////////////////品牌数据删除
 
 </script>
 
